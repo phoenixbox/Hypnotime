c6850eff02057deb5309f49ce8a9d35d4d68c359
phoenixbox <shane@quickleft.com>
2013-11-20 10:09:54 -0700

    bottom bar UITabBarController added to switch between the two controllers of its array

--- Hypnotime/HypnotimeAppDelegate.m
+++ Hypnotime/HypnotimeAppDelegate.m
@@ -17,9 +17,15 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
+    HypnosisViewController *hvc = [[HypnosisViewController alloc]init];
     TimeViewController *tvc = [[TimeViewController alloc]init];
+    
+    UITabBarController *tabBarController = [[UITabBarController alloc]init];
+    
+    NSArray *viewControllers = [NSArray arrayWithObjects:hvc, tvc, nil];
+    [tabBarController setViewControllers:viewControllers];
 
-    [[self window] setRootViewController:tvc];
+    [[self window] setRootViewController:tabBarController];
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
