ad9023424488e0abb87585da7e4d5f704050f6d2
phoenixbox <shane@quickleft.com>
2013-11-20 07:22:20 -0700

    Initial Commit

---
+++ Hypnotime/HypnotimeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnotimeAppDelegate.h
+//  Hypnotime
+//
+//  Created by Shane Rogers on 11/20/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnotimeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnotime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnotime
+//
+//  Created by Shane Rogers on 11/20/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnotimeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnotimeAppDelegate class]));
+    }
+}
